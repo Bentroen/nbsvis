f791a4c2c5e9472c8ba4e011cebe74e7abdb3150
Bernardo Costa <bemcdc@gmail.com>
2025-03-11 15:31:20 -0300

    chore: add `eslint-plugin-unused-imports`

--- eslint.config.js
+++ eslint.config.js
@@ -1,52 +1,45 @@
-import pluginJs from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
-import prettier from "eslint-config-prettier";
-import importPlugin from "eslint-plugin-import";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       globals: globals.browser,
       parser: tsParser,
-      sourceType: "module",
+      sourceType: 'module',
     },
     plugins: {
-      //    prettier: prettierPlugin,
-      //import: importPlugin,
+      import: importPlugin,
+      'unused-imports': unusedImports,
     },
     rules: {
-      semi: ["warn", "always"],
-      quotes: ["warn", "double"],
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      //"prettier/prettier": [
-      //  "warn",
-      //  {
-      //    semi: true,
-      //    singleQuote: false,
-      //    trailingComma: "all",
-      //    printWidth: 100,
-      //    tabWidth: 2,
-      //  },
-      //],
-      //"import/order": [
-      //  "warn",
-      //  {
-      //    groups: [
-      //      "builtin", // Node.js built-in modules (fs, path, etc.)
-      //      "external", // npm packages (react, express, etc.)
-      //      "internal", // Aliases like @utils, @components
-      //      ["parent", "sibling", "index"], // Relative imports
-      //    ],
-      //    "newlines-between": "always",
-      //    alphabetize: { order: "asc", caseInsensitive: true },
-      //  },
-      //],
+      semi: ['warn', 'always'],
+      quotes: ['warn', 'double'],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin', // Node.js built-in modules (fs, path, etc.)
+            'external', // npm packages (react, express, etc.)
+            'internal', // Aliases like @utils, @components
+            ['parent', 'sibling', 'index'], // Relative imports
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/no-unresolved': ['warn'],
+      'unused-imports/no-unused-imports': 'warn',
     },
   },
   prettier,
