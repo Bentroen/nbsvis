46eb2f2123a4942e10079b4899e06b2f81f5e1a6
Bernardo Costa <bemcdc@gmail.com>
2025-03-11 05:44:26 -0300

    chore: pixi import syntax and window setup

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
-import * as PIXI from 'pixi.js';
+import { Application, Assets, Graphics, Sprite } from 'pixi.js';
 
-const app = new PIXI.Application();
+const app = new Application();
 
 await app.init({
-  width: 800,
-  height: 600,
   backgroundColor: 0x1099bb,
+  resizeTo: window,
 });
 
 document.getElementById('app')?.appendChild(app.view);
 
-const graphics = new PIXI.Graphics();
+const graphics = new Graphics();
 graphics.rect(100, 100, 200, 200);
 graphics.fill(0xff0000);
 app.stage.addChild(graphics); // Add the graphics to the PixiJS stage
