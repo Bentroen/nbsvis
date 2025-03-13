5927704ab520a81317cf00e94048428cd4d64e21
Bentroen <bemcdc@gmail.com>
2025-03-13 05:45:06 -0300

    refactor: remove pixi.js initial demo/test code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Assets, Container, Graphics, Sprite, Text, TextureStyle } from 'pixi.js';
+import { Application, Container, Text, TextureStyle } from 'pixi.js';
 
 import { loadInstruments, playSong } from './audio';
 import { loadSong, NoteManager } from './note';
@@ -20,28 +20,6 @@ await app.init({
 });
 
 appContainer.appendChild(app.canvas);
-
-const graphics = new Graphics();
-graphics.rect(100, 100, 200, 200);
-graphics.fill(0xff0000);
-app.stage.addChild(graphics); // Add the graphics to the PixiJS stage
-
-const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
-
-const noteBlockSprite = new Sprite(texture);
-noteBlockSprite.position.set(250, 100);
-app.stage.addChild(noteBlockSprite);
-
-noteBlockSprite.anchor.set(0.5);
-
-noteBlockSprite.x = app.screen.width / 2;
-noteBlockSprite.y = app.screen.height / 2;
-
-noteBlockSprite.scale.set(2);
-
-app.ticker.add((time) => {
-  noteBlockSprite.rotation += 0.1 * time.deltaTime;
-});
 
 //----------------------------------------------------------------
 
