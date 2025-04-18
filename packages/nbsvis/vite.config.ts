1ddf3f4d305eed471bac0723d4696fa0131a267d
Bentroen <bemcdc@gmail.com>
2025-04-18 13:52:37 -0300

    build: set up different vite configs for dev and prod

--- packages/nbsvis/vite.config.ts
+++ packages/nbsvis/vite.config.ts
@@ -1,30 +1,27 @@
 import { resolve } from 'pathe';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig({
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'assets/*',
-          dest: 'assets',
-        },
-      ],
-    }),
-  ],
-  build: {
-    target: 'esnext',
-    sourcemap: false,
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'nbsvis',
-      fileName: 'nbsvis',
-      formats: ['es'], // TODO: umd, cjs (remove top-level await)
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
+    build: {
+      target: 'ESNext',
+      sourcemap: mode === 'development',
+      minify: mode === 'production' ? 'oxc' : false,
+      lib: {
+        entry: resolve(__dirname, 'src/main.ts'),
+        name: 'nbsvis',
+        fileName: 'nbsvis',
+        formats: ['es'], // TODO: umd, cjs (remove top-level await)
+      },
+      rollupOptions: {
+        external: ['pixi.js'],
+      },
     },
-  },
+  };
 });
