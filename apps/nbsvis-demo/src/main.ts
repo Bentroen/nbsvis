32e9cfc6d964bd0cd11dcdca5c98af368a4f2a19
Bentroen <bemcdc@gmail.com>
2025-04-25 21:42:21 -0300

    fix: wrong NBW api url (oops)

--- apps/nbsvis-demo/src/main.ts
+++ apps/nbsvis-demo/src/main.ts
@@ -57,7 +57,9 @@ async function loadSong() {
     console.log('Loading song from URL:', url);
     const songId = url.split('/').pop();
     const serverUrl =
-      process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://noteblock.world';
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:4000'
+        : 'https://api.noteblock.world';
     const songDownloadUrl = `${serverUrl}/api/v1/song/${songId}/open`;
     console.log('Downloading song from Note Block World:', songDownloadUrl);
     const response = await fetch(songDownloadUrl, {
