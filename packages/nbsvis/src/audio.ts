6852699fefad97da3900a061ae2c2aad9752b750
Bentroen <29354120+Bentroen@users.noreply.github.com>
2026-01-29 00:25:43 -0300

    fix(audio): re-add instrument pitch offset to playback

--- packages/nbsvis/src/audio.ts
+++ packages/nbsvis/src/audio.ts
@@ -44,7 +44,8 @@ function getNoteEvents(song: Song) {
       // song -> notes
       const tick = parseInt(tickStr);
       const instrument = note.instrument;
-      const key = note.key + note.pitch / 100;
+      const instrumentKeyOffset = 45 - song.instruments.loaded[instrument].key + 45;
+      const key = note.key - instrumentKeyOffset + note.pitch / 100;
       const velocity = ((note.velocity / 100) * layer.volume) / 100;
       const panning = (layer.stereo === 0 ? note.panning : (note.panning + layer.stereo) / 2) / 100;
 
